8f4cf03006e1dffc4692be187b68f166cb528226
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-21 16:07:03 -0300

    feat: export location permission helpers

--- src/pioneiro_pro/services/__init__.py
+++ src/pioneiro_pro/services/__init__.py
@@ -2,6 +2,13 @@ from .backup_service import BackupService
 from .cronometro_service import CronometroService
 from .exportacao_service import ExportacaoService
 from .lembrete_service import LembreteService
+from .location_service import (
+    LocationAccessResult,
+    background_location_granted,
+    foreground_location_granted,
+    get_current_position_with_permission,
+    normalize_permission_status,
+)
 from .proximidade_service import ProximidadeService
 
 __all__ = [
@@ -9,5 +16,10 @@ __all__ = [
     "CronometroService",
     "ExportacaoService",
     "LembreteService",
+    "LocationAccessResult",
+    "background_location_granted",
+    "foreground_location_granted",
+    "get_current_position_with_permission",
+    "normalize_permission_status",
     "ProximidadeService",
 ]
