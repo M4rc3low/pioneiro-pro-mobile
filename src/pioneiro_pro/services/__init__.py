2a2c9d98a9de2667df1ae58fa39bdf5483c5aeb2
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-21 12:30:48 -0300

    chore: exportar serviços finais

--- src/pioneiro_pro/services/__init__.py
+++ src/pioneiro_pro/services/__init__.py
@@ -1,3 +1,11 @@
+from .backup_service import BackupService
 from .cronometro_service import CronometroService
+from .exportacao_service import ExportacaoService
+from .lembrete_service import LembreteService
 
-__all__ = ["CronometroService"]
+__all__ = [
+    "BackupService",
+    "CronometroService",
+    "ExportacaoService",
+    "LembreteService",
+]
