d2d6983f816fb0c21b176404b934bd9d936bf044
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-21 12:23:05 -0300

    chore: exportar perfil de estudante

--- src/pioneiro_pro/pages/__init__.py
+++ src/pioneiro_pro/pages/__init__.py
@@ -1,6 +1,7 @@
 from .agenda import agenda_view
 from .configuracoes import configuracoes_view
 from .dashboard import dashboard_view
+from .estudante_detalhes import estudante_detalhes_view
 from .estudantes import estudantes_view
 from .registrar import registrar_view
 from .relatorios import relatorios_view
@@ -9,6 +10,7 @@ __all__ = [
     "agenda_view",
     "configuracoes_view",
     "dashboard_view",
+    "estudante_detalhes_view",
     "estudantes_view",
     "registrar_view",
     "relatorios_view",
