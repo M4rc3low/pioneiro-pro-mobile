effa1d383f3ca839d49a1a4504922fc5fb6a12c2
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-21 12:31:30 -0300

    chore: exportar onboarding

--- src/pioneiro_pro/pages/__init__.py
+++ src/pioneiro_pro/pages/__init__.py
@@ -3,6 +3,7 @@ from .configuracoes import configuracoes_view
 from .dashboard import dashboard_view
 from .estudante_detalhes import estudante_detalhes_view
 from .estudantes import estudantes_view
+from .onboarding import onboarding_view
 from .registrar import registrar_view
 from .relatorios import relatorios_view
 
@@ -12,6 +13,7 @@ __all__ = [
     "dashboard_view",
     "estudante_detalhes_view",
     "estudantes_view",
+    "onboarding_view",
     "registrar_view",
     "relatorios_view",
 ]
