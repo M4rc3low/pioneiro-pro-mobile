ebce20b6d3384f58507bac9cbbb97c9beb340508
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-21 12:19:14 -0300

    chore: exportar novos repositórios

--- src/pioneiro_pro/repositories/__init__.py
+++ src/pioneiro_pro/repositories/__init__.py
@@ -1,4 +1,11 @@
 from .atividade_repository import AtividadeRepository
+from .configuracao_repository import ConfiguracaoRepository
 from .estudante_repository import EstudanteRepository
+from .visita_repository import VisitaRepository
 
-__all__ = ["AtividadeRepository", "EstudanteRepository"]
+__all__ = [
+    "AtividadeRepository",
+    "ConfiguracaoRepository",
+    "EstudanteRepository",
+    "VisitaRepository",
+]
