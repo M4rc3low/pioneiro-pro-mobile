ef0b300b762330a1529a99f90e7f9172f2394f6b
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-21 12:09:52 -0300

    chore: iniciar páginas do aplicativo

---
+++ src/pioneiro_pro/pages/__init__.py
@@ -0,0 +1,15 @@
+from .agenda import agenda_view
+from .configuracoes import configuracoes_view
+from .dashboard import dashboard_view
+from .estudantes import estudantes_view
+from .registrar import registrar_view
+from .relatorios import relatorios_view
+
+__all__ = [
+    "agenda_view",
+    "configuracoes_view",
+    "dashboard_view",
+    "estudantes_view",
+    "registrar_view",
+    "relatorios_view",
+]
